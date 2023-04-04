597e4c2b6bb9cac12dc1c2a104388bd4c5452c46
Celeste Layne <layne.celeste@gmail.com>
2023-04-04 17:52:21 -0400

    event score starter

---
+++ exercises/event-score-settimeout/main.js
@@ -0,0 +1,30 @@
+console.log("This code is loading the JavaScript file");
+
+let materials = ["wood", "brick", "broken dishes", "dust", "leaves", "grass"];
+
+let places = [
+  "cold, windy climate",
+  "desert",
+  "deserted airport",
+  "deserted church",
+  "deserted factory",
+  "hot climate",
+  "metropolis"
+];
+
+let people = [
+  "collectors of all types",
+  "fishermen and families",
+  "french and german speaking people",
+  "old friends",
+  "horses and birds",
+  "little boys",
+  "lovers"
+];
+
+let thing = [
+  "natural light",
+  "all available lighting",
+  "candles",
+  "electricity"
+];
