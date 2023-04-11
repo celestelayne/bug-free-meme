152051449325c664466ead6ac6cdf540629cfa15
Celeste Layne <layne.celeste@gmail.com>
2023-04-11 19:22:00 -0400

    remove solution from starter code

--- exercises/search-array-names/main.js
+++ exercises/search-array-names/main.js
@@ -22,63 +22,31 @@ const people = [
   { name: "uff" },
   { name: "violet" },
   { name: "wyatt" },
-  { name: "x" },
+  { name: "xavier" },
   { name: "yadri" },
   { name: "zack" }
 ];
 
-// get search bar element
 const searchBar = document.querySelector("input");
 const ul = document.querySelector("ul");
 const clearButton = document.getElementById('clear')
 
 //---------- RENDER NAMES TO PAGE
-function renderNamesToPage(people) {
-  for (let i = 0; i < people.length; i++) {
-    // creating a li element for each result item
-    let list_item = document.createElement("li");
-    // adding a class to each item of the results
-    list_item.classList.add("result-item");
-    // grabbing the name of the current point of the loop and adding the name as the list item's text
-    list_item.textContent = people[i].name;
-    // appending the result item to the list
-    ul.appendChild(list_item);
-  }
-}
+function renderNamesToPage(data) {}
 renderNamesToPage(people);
 
-//---------- SEARCH NAMES FOR SPECIFIC NAME
-function searchNames(e) {
-  const searchString = e.target.value.toLowerCase();
-  console.log("line 48: ", searchString);
-  
-  const searchedName = people.filter(function (person) {
-    if (searchString && searchString.trim().length > 0) {
-      return person.name.includes(searchString);
-    }
-  });
-  displaySearched(searchedName);
-}
-
+//---------- SEARCH DATASET FOR SPECIFIC NAME
+function searchNames(e) {}
 searchBar.addEventListener("keyup", searchNames);
 
-function displaySearched(name) {
-  console.log("line 57: ", name);
-  clearList();
-  for (let i = 0; i < name.length; i++) {
-    let list_item = document.createElement("li");
-    list_item.textContent = name[i].name;
-    ul.appendChild(list_item);
-  }
-}
+//---------- DISPLAY ONLY THE SPECIFIC NAME
+function displaySearched(name) {}
 
-function clearList() {
-  console.log("clear this list first");
-  ul.innerHTML = "";
-}
+//---------- CLEAR LIST BEFORE RENDERING SPECIFIC NAME
+function clearList() {}
 
+//---------- CLEAR FROM WITH BUTTON
 function clearSearchForm() {
   searchBar.reset()
 }
-
 clearButton.addEventListener('click', clearSearchForm)
