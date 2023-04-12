cb57f7afd44c01f335c0348b8368914de2031a91
Celeste Layne <layne.celeste@gmail.com>
2023-04-11 20:05:04 -0400

    search array of names (solution)

--- exercises/search-array-names/main.js
+++ exercises/search-array-names/main.js
@@ -32,20 +32,59 @@ const ul = document.querySelector("ul");
 const clearButton = document.getElementById('clear')
 
 //---------- RENDER NAMES TO PAGE
-function renderNamesToPage(data) {}
+function renderNamesToPage(results) {
+  // [1] iterate of the dataset
+  for(let i = 0; i < results.length; i++){
+    // [2] create the list item
+    let listItem = document.createElement('li');
+    // [3] add a class to the list item
+    listItem.classList.add('text-color')
+    // [4] grab each name and add it to the list item
+    listItem.textContent = results[i].name
+    // [5] append the list time to the unordered list
+    ul.appendChild(listItem)
+
+  }
+}
 renderNamesToPage(people);
 
 //---------- SEARCH DATASET FOR SPECIFIC NAME
-function searchNames(e) {}
+function searchNames(event) {
+  console.log(event.target.value)
+  let searchQuery = event.target.value.toLowerCase(); // jacob
+
+  const searchedName = people.filter(function(person){
+    if(searchQuery){
+      return person.name.includes(searchQuery)
+    }
+  })
+
+  displaySearched(searchedName)
+}
 searchBar.addEventListener("keyup", searchNames);
 
 //---------- DISPLAY ONLY THE SPECIFIC NAME
-function displaySearched(name) {}
+function displaySearched(names) {
+  // console.log('search value: ', names[0])
+  clearList()
+  // iterate over the searched names
+  for(let i = 0; i < names.length; i++){
+    // create list item
+    let listItem = document.createElement('li')
+    // grab each name and add it to the list item
+    listItem.textContent = names[i].name
+    // append the search results to the unordered list
+    ul.appendChild(listItem)
+  }
+
+}
 
 //---------- CLEAR LIST BEFORE RENDERING SPECIFIC NAME
-function clearList() {}
+function clearList() {
+  ul.innerHTML = ""
+}
 
-//---------- CLEAR FROM WITH BUTTON
+//---------- CLEAR FORM WITH BUTTON
 function clearSearchForm() {
   searchBar.reset()
 }
