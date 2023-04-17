c6024b35b27504810cdcfb08062dc214537f6eba
Celeste Layne <layne.celeste@gmail.com>
2023-04-16 20:07:53 -0400

    create dedicated projects page

---
+++ projects.js
@@ -0,0 +1,68 @@
+console.log('this works')
+
+const students = [
+  {
+    name: 'Priscilla Auyeung',
+    program: ['MArch'],
+    thumbnail: '',
+    github: {
+      repo: '',
+      midterm: 'https://pla2116.github.io/spicy-cabbage/PROJECTS/01/index.html',
+      final: ''
+    }
+  },
+  {
+    name: 'Nicole Biewenga',
+    program: ['MArch', 'MS RED'],
+    thumbnail: '',
+    github: {
+      repo: '',
+      midterm: 'https://pla2116.github.io/spicy-cabbage/PROJECTS/01/index.html',
+      final: ''
+    }
+  },
+  {
+    name: 'Sixue Chen',
+    program: ['MArch'],
+    thumbnail: '',
+    github: {
+      repo: '',
+      midterm: 'https://pla2116.github.io/spicy-cabbage/PROJECTS/01/index.html',
+      final: ''
+    }
+  },
+  {
+    name: 'Ridhi Chopra',
+    program: ['MArch'],
+    thumbnail: '',
+    github: {
+      repo: '',
+      midterm: 'https://pla2116.github.io/spicy-cabbage/PROJECTS/01/index.html',
+      final: ''
+    }
+  },
+  {
+    name: 'Ruonan Du',
+    program: ['MArch'],
+    thumbnail: '',
+    github: {
+      repo: '',
+      midterm: 'https://pla2116.github.io/spicy-cabbage/PROJECTS/01/index.html',
+      final: ''
+    }
+  },
+  {
+    name: 'Shuhan Fang',
+    program: ['MArch'],
+    thumbnail: '',
+    github: {
+      repo: '',
+      midterm: 'https://pla2116.github.io/spicy-cabbage/PROJECTS/01/index.html',
+      final: ''
+    }
+  }
+]
+
+// render the screenshots as a gallery
+
+// drag and drop anywhere on the page
